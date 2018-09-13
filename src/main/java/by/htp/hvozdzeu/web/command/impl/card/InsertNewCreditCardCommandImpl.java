ffff0e61d379de94e0721a7c1bdb22dfac63e3dd
ALIAKSANDR <aliaksandr.hvozdzeu@gmail.com>
2018-09-13 15:12:45 +0300

    Refactoring, create simple autotest on selenium

--- src/main/java/by/htp/hvozdzeu/web/command/impl/card/InsertNewCreditCardCommandImpl.java
+++ src/main/java/by/htp/hvozdzeu/web/command/impl/card/InsertNewCreditCardCommandImpl.java
@@ -20,9 +20,9 @@ import static by.htp.hvozdzeu.util.MailSender.mailSender;
 
 public class InsertNewCreditCardCommandImpl implements BaseCommand {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(InsertNewCreditCardCommandImpl.class);
     private ICreditCardService iCreditCardService = ServiceFactory.getCreditCardService();
     private IBankAccountService iBankAccountService = ServiceFactory.getBankAccountService();
-    private static final Logger LOGGER = LoggerFactory.getLogger(InsertNewCreditCardCommandImpl.class);
 
     @Override
     public String executeCommand(HttpServletRequest request) throws CommandException {
@@ -38,51 +38,65 @@ public class InsertNewCreditCardCommandImpl implements BaseCommand {
         String creditCardType = request.getParameter("creditCardType");
         String secretCode = request.getParameter("secretCode");
 
-        LOGGER.debug("First step. Create credit card.");
-        String validDate = cardValidMonth + "/" + cardValidYear;
-        CreditCard creditCard = new CreditCard.Builder()
-                .client(userId)
-                .cardNumber(cardNumber)
-                .cardFirstName(firstNameCreditCard)
-                .cardLastName(lastNameCreditCard)
-                .validDate(validDate)
-                .typeCard(TypeCard.valueOf(creditCardType))
-                .verifyCode(secretCode)
-                .block(false)
-                .build();
+        CreditCard checkCreditCard = iCreditCardService.findByCreditCardNumber(cardNumber);
 
-        Long newCreditCardId = iCreditCardService.createReturnId(creditCard);
+        if (checkCreditCard != null) {
 
-        if(newCreditCardId != null && newCreditCardId != 0){
+            request.getSession().setAttribute("messageCheckCreditCard", "Such a credit card exists.");
 
-            String numberAccountFirstPart = cardNumber.split(" ")[0];
-            String numberAccountSecondPart = cardNumber.split(" ")[3];
-            String nameAccount = "BA_" + numberAccountFirstPart + numberAccountSecondPart;
-
-            LOGGER.debug("Second step. Create bank account.");
-            BankAccount bankAccount = new BankAccount.Builder()
-                    .creditCard(newCreditCardId)
-                    .nameAccount(nameAccount)
-                    .statusBankAccount(false)
-                    .balanceBankAccount(BigDecimal.valueOf(0.00))
-                    .available(false)
-                    .build();
-
-            iBankAccountService.create(bankAccount);
-
-            String emailToReply = user.getEmail();
-            String subjectToReply = "Information about insert new credit card.";
-            String messageToReply = "Hello. " +
-                    "Your card # " + creditCard.getCardNumber() + " has been inserted. " +
-                    "And create new bank account [" + nameAccount + "] with code your locale position. " +
-                    "For additional information, please return to administrator.";
-            mailSender(request, emailToReply, subjectToReply, messageToReply);
-
+            return PagePathConstantPool.ADD_NEW_CREDIT_CARD;
 
         } else {
-            request.getSession().setAttribute("messageErrorInsertNewCreditCard", "Error insert credit cards");
+
+            LOGGER.debug("First step. Create credit card.");
+            String validDate = cardValidMonth + "/" + cardValidYear;
+            CreditCard creditCard = new CreditCard.Builder()
+                    .client(userId)
+                    .cardNumber(cardNumber)
+                    .cardFirstName(firstNameCreditCard)
+                    .cardLastName(lastNameCreditCard)
+                    .validDate(validDate)
+                    .typeCard(TypeCard.valueOf(creditCardType))
+                    .verifyCode(secretCode)
+                    .block(false)
+                    .build();
+
+            Long newCreditCardId = iCreditCardService.createReturnId(creditCard);
+
+            if (newCreditCardId != null && newCreditCardId != 0) {
+
+                String numberAccountFirstPart = cardNumber.split(" ")[0];
+                String numberAccountSecondPart = cardNumber.split(" ")[3];
+                String nameAccount = "BA_" + numberAccountFirstPart + numberAccountSecondPart;
+
+                LOGGER.debug("Second step. Create bank account.");
+                BankAccount bankAccount = new BankAccount.Builder()
+                        .creditCard(newCreditCardId)
+                        .nameAccount(nameAccount)
+                        .statusBankAccount(false)
+                        .balanceBankAccount(BigDecimal.valueOf(0.00))
+                        .available(false)
+                        .build();
+
+                iBankAccountService.create(bankAccount);
+
+                String emailToReply = user.getEmail();
+                String subjectToReply = "Information about insert new credit card.";
+                String messageToReply = "Hello. " +
+                        "Your card # " + creditCard.getCardNumber() + " has been inserted. " +
+                        "And create new bank account [" + nameAccount + "] with code your locale position. " +
+                        "For additional information, please return to administrator.";
+                mailSender(request, emailToReply, subjectToReply, messageToReply);
+
+                return PagePathConstantPool.REDIRECT_LIST_CARD_CLIENT;
+
+            } else {
+                request.getSession().setAttribute("messageErrorInsertNewCreditCard", "Error insert credit cards");
+                return PagePathConstantPool.ADD_NEW_CREDIT_CARD;
+            }
+
         }
 
-        return PagePathConstantPool.REDIRECT_LIST_CARD_CLIENT;
+
     }
 }
